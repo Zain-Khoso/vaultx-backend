b0fb33520a10c77aa9d92aed9aae8bac581ae779
Zain-Khoso <zain.khoso.dev@gmail.com>
2025-07-12 13:44:46 +0500

    mongodb connection.

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,19 @@
 import express, { Application, Request, Response } from "express";
 import "dotenv/config";
+import { connectDB } from "./db";
+
 const app: Application = express();
 const PORT = process.env.PORT || 8000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get("/", async (req, res) => {
+app.get("/", async (req: Request, res: Response) => {
   res.status(200).json({ message: "success" });
 });
 
-app.listen(PORT, () => {
-  console.log("Server is running on port", PORT);
-});
+connectDB().then(() =>
+  app.listen(PORT, () => {
+    console.log("Server is running on port: ", PORT);
+  })
+);
